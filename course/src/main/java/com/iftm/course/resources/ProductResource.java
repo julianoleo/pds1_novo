3a8c2575b53d3d6701a23485b2e7350e1b115d3a
julianoleo <50123832+julianoleo@users.noreply.github.com>
2020-12-15 20:09:27 -0300

    Challenge: CategoryDTO, ProductDTO

--- course/src/main/java/com/iftm/course/resources/ProductResource.java
+++ course/src/main/java/com/iftm/course/resources/ProductResource.java
@@ -1,15 +1,21 @@
 package com.iftm.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.iftm.course.entities.Product;
+import com.iftm.course.dto.ProductDTO;
 import com.iftm.course.services.ProductService;
 
 @RestController
@@ -20,14 +26,14 @@ public class ProductResource {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll() {		
-		List<Product> list = service.findAll();
-		return ResponseEntity.ok().body(list);		
+	public ResponseEntity<List<ProductDTO>> findAll() {	
+		List<ProductDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}	
 }
