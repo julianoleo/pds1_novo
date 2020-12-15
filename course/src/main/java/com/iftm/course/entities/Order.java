ce5f224e4097e7e483dc2f85df8af828d808c442
julianoleo <50123832+julianoleo@users.noreply.github.com>
2020-12-15 20:56:07 -0300

    OrderDTO

--- course/src/main/java/com/iftm/course/entities/Order.java
+++ course/src/main/java/com/iftm/course/entities/Order.java
@@ -47,7 +47,7 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
