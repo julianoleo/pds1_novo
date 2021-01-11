189acf2df74e0e5c5986d86fb6611d2e225b1a86
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-11 14:12:47 -0300

    Custom validation response

--- course/src/main/java/com/iftm/course/resources/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/com/iftm/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,33 +1,40 @@
 package com.iftm.course.resources.exceptions;
-
 import java.time.Instant;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.iftm.course.services.exceptions.DatabaseException;
 import com.iftm.course.services.exceptions.ResourceNotFoundException;
-
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		String error  = "Resource not Found";
+		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 	
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request) {
 		String error = "Database error";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		String error = "Validation error";
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		for (FieldError fe : e.getBindingResult().getFieldErrors() ) err.addError(fe.getField(), fe.getDefaultMessage());
 		return ResponseEntity.status(status).body(err);
 	}
 }
