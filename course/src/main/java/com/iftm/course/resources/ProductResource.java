c7c1f59d5afeb73f6e4c97e670d5ab2a759eb2c9
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-11 11:48:49 -0300

    Product update

--- course/src/main/java/com/iftm/course/resources/ProductResource.java
+++ course/src/main/java/com/iftm/course/resources/ProductResource.java
@@ -5,15 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.iftm.course.dto.ProductCategoriesDTO;
 import com.iftm.course.dto.ProductDTO;
 import com.iftm.course.services.ProductService;
-
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
@@ -30,11 +29,16 @@ public class ProductResource {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
 		return ResponseEntity.ok().body(productService.findById(id));
 	}
-
+	
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
 		ProductDTO newDto = productService.insert(dto);
 		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri())
 				.body(newDto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto) {
+		return ResponseEntity.ok().body(productService.update(id, dto));
 	}
 }
--- course/src/main/java/com/iftm/course/services/ProductService.java
+++ course/src/main/java/com/iftm/course/services/ProductService.java
@@ -2,10 +2,11 @@ package com.iftm.course.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.iftm.course.dto.CategoryDTO;
 import com.iftm.course.dto.ProductCategoriesDTO;
 import com.iftm.course.dto.ProductDTO;
@@ -13,32 +14,47 @@ import com.iftm.course.entities.Product;
 import com.iftm.course.repositories.CategoryRepository;
 import com.iftm.course.repositories.ProductRepository;
 import com.iftm.course.services.exceptions.ResourceNotFoundException;
-
 @Service
 public class ProductService {
-
 	@Autowired
 	private ProductRepository productRepository;
-
+	
 	@Autowired
 	private CategoryRepository categoryRepository;
-
 	public List<ProductDTO> findAll() {
 		return productRepository.findAll().stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 	}
 	public ProductDTO findById(Long id) {
 		return new ProductDTO(productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id)));
 	}
-
 	@Transactional
 	public ProductDTO insert(ProductCategoriesDTO dto) {
 		Product entity = dto.toEntity();
 		setProductCategories(entity, dto.getCategories());
 		return new ProductDTO(productRepository.save(entity));
 	}
-
+	
 	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
 		entity.getCategories().clear();
 		for (CategoryDTO dto : categories) entity.getCategories().add(categoryRepository.getOne(dto.getId()));
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO dto) {
+		try {
+			Product entity = productRepository.getOne(id);
+			updateData(entity, dto);
+			return new ProductDTO(productRepository.save(entity));
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		if (dto.getCategories() != null && dto.getCategories().size() > 0) setProductCategories(entity, dto.getCategories());
 	}
 } 
