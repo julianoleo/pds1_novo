3bc135692f03d4be6f98d834149f2ee0c9471d98
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-11 14:19:55 -0300

    Custom user insert validation: unique email

--- course/src/main/java/com/iftm/course/repositories/UserRepository.java
+++ course/src/main/java/com/iftm/course/repositories/UserRepository.java
@@ -1,9 +1,8 @@
 package com.iftm.course.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.iftm.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
 
-	
+	User findByEmail(String email);
 }
