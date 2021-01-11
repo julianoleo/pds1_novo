f32efc891ee7e27f650a3f95d545ba27149e2e20
Juliano Leocádio Batista <julianoleo@gmail.com>
2021-01-11 14:06:07 -0300

    User basic validation - Bean Validation

--- course/src/main/java/com/iftm/course/dto/UserInsertDTO.java
+++ course/src/main/java/com/iftm/course/dto/UserInsertDTO.java
@@ -2,20 +2,34 @@ package com.iftm.course.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.iftm.course.entities.User;
 
-public class UserInsertDTO implements Serializable{
+public class UserInsertDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+
+	@NotEmpty(message = "can't be emply")
+	@Length(min = 5, max = 80, message = "Length must be between 5 and 80!")
 	private String name;
+
+	@NotEmpty(message = "can't be empty")
+	@Email
 	private String email;
+
+	@NotEmpty(message = "can't be empty")
+	@Length(min = 8, max = 20, message = "Length must be between 8 and 20")
 	private String phone;
+
+	@NotEmpty(message = "can't be empty")
 	private String password;
 
-	public UserInsertDTO() {
-	}
-
+	public UserInsertDTO() {}
 	public UserInsertDTO(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -32,45 +46,38 @@ public class UserInsertDTO implements Serializable{
 		this.phone = entity.getPhone();
 		this.password = entity.getPassword();
 	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 	
 	public User toEntity() {
--- course/src/main/java/com/iftm/course/resources/UserResource.java
+++ course/src/main/java/com/iftm/course/resources/UserResource.java
@@ -1,7 +1,8 @@
 package com.iftm.course.resources;
 
-import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.iftm.course.dto.UserDTO;
 import com.iftm.course.dto.UserInsertDTO;
-import com.iftm.course.entities.User;
 import com.iftm.course.services.UserService;
-
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -33,8 +31,9 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(userService.findById(id));
 	}
+
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
 		return ResponseEntity
 				.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri())
 				.body(userService.insert(dto));
